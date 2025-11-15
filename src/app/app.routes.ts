e0b82458adcf0e148f06febd2f782a92d93fb812
Riccardo Reho <riccardoreho95@gmail.com>
2025-11-15 15:47:35 +0100

    comment outreach

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './components/home/home';
 import { TalksComponent } from './components/talks/talks';
 import { PublicationsComponent } from './components/publications/publications';
 import { AboutComponent } from './components/about/about';
-import { OutreachComponent } from './components/outreach/outreach';
+// import { OutreachComponent } from './components/outreach/outreach';
 import { QaComponent } from './components/qa/qa';
 import { BlogComponent } from './components/blog/blog';
 
@@ -13,7 +13,7 @@ export const routes: Routes = [
   { path: 'talks', component: TalksComponent },
   { path: 'publications', component: PublicationsComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'outreach', component: OutreachComponent },
+  // { path: 'outreach', component: OutreachComponent },
   { path: 'qa', component: QaComponent },
   { path: 'blog', component: BlogComponent },
   { path: '**', redirectTo: '/home' }
